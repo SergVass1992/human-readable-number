b620879cfbaf9bbd2d1c3deb7109474758856ab1
Сергей <56017574+SERgey1312@users.noreply.github.com>
2021-01-28 12:35:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
-}
+  	
+    let hundr = Math.floor(number / 100);
+    let tens = Math.floor((number - hundr * 100) / 10);
+    let ones = number - hundr * 100 - tens * 10;
+
+	if (number == 0) {
+        return 'zero';
+    }
+
+    if (tens == 1) {
+        tens = 0;
+        ones = number - hundr * 100;
+    }
+
+    let onesArr = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tensArr = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+	let hundrArr = hundr ? onesArr[hundr] + ' hundred ' : '';
+
+    return (hundrArr + tensArr[tens] + onesArr[ones]).trim();
+};
+
